49b21c05c74e28d2182fc9a13e17fa609a97d475
Samip_Maharjan <samip1317@gmail.com>
2023-04-03 20:35:56 +0545

    TicTacToe, game completed but additional features remains

--- TicTacToe/src/App.jsx
+++ TicTacToe/src/App.jsx
@@ -2,32 +2,81 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Square from './Components/Square'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [ board, setBoard ] = useState([
+    [ "", "", "" ], 
+    [ "", "", "" ],
+    [ "", "", "" ]
+  ]);
+
+  const [ noughtOrCross, setNoughtOrCross ] = useState('X');
+
+  const updateSquare = ( row, column ) => {
+
+    if ( board[ row ][ column ] == "" ){
+      const newArray = [ ...board ];
+      newArray[ row ][ column ] = noughtOrCross;
+      setBoard( newArray );
+
+      checkForWin( noughtOrCross );
+      // assigning the array element associated with the square to nought or a cross.
+
+      noughtOrCross == 'X' ? setNoughtOrCross('O') : setNoughtOrCross('X');
+      // changing the nought or cross.
+    }
+    else{
+      alert("The square is already taken ")
+    }
+
+    
+  }
+
+  const checkForWin = ( noughtOrCross ) => {
+    if (
+      (board[ 0 ][ 0 ] == noughtOrCross && board[ 0 ][ 1 ] == noughtOrCross && board[ 0 ][ 2 ] == noughtOrCross) ||
+      (board[ 1 ][ 0 ] == noughtOrCross && board[ 1 ][ 1 ] == noughtOrCross && board[ 1 ][ 2 ] == noughtOrCross) ||
+      (board[ 2 ][ 0 ] == noughtOrCross && board[ 2 ][ 1 ] == noughtOrCross && board[ 2 ][ 2 ] == noughtOrCross) ||
+      (board[ 0 ][ 0 ] == noughtOrCross && board[ 1 ][ 0 ] == noughtOrCross && board[ 2 ][ 0 ] == noughtOrCross) ||
+      (board[ 0 ][ 1 ] == noughtOrCross && board[ 1 ][ 1 ] == noughtOrCross && board[ 2 ][ 1 ] == noughtOrCross) ||
+      (board[ 0 ][ 2 ] == noughtOrCross && board[ 1 ][ 2 ] == noughtOrCross && board[ 2 ][ 2 ] == noughtOrCross) ||
+      (board[ 0 ][ 0 ] == noughtOrCross && board[ 1 ][ 1 ] == noughtOrCross && board[ 2 ][ 2 ] == noughtOrCross) ||
+      (board[ 0 ][ 2 ] == noughtOrCross && board[ 1 ][ 1 ] == noughtOrCross && board[ 2 ][ 0 ] == noughtOrCross)
+    )
+    {  
+      setTimeout( () => {
+        alert(`${noughtOrCross} has won the game.`);
+      }, 300);
+    }
+  } 
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="board">
+
+        <div className="row row1">
+          <Square value={board[0][0]} chooseSquare={ () => { updateSquare( 0, 0 )}}/>
+          <Square value={board[0][1]} chooseSquare={ () => { updateSquare( 0, 1 ) }}/>
+          <Square value={board[0][2]} chooseSquare={ () => { updateSquare( 0, 2 ) }}/>
+        </div>
+        {/* Row 1  */}
+
+        <div className="row row2">
+          <Square value={board[1][0]} chooseSquare={ () => { updateSquare( 1, 0 ) }}/>
+          <Square value={board[1][1]} chooseSquare={ () => { updateSquare( 1, 1 ) }}/>
+          <Square value={board[1][2]} chooseSquare={ () => { updateSquare( 1, 2 ) }}/>
+        </div>
+        {/* Row 2 */}
+        
+        <div className="row row3">
+          <Square value={board[2][0]} chooseSquare={ () => { updateSquare( 2, 0 ) }}/>
+          <Square value={board[2][1]} chooseSquare={ () => { updateSquare( 2, 1 ) }}/>
+          <Square value={board[2][2]} chooseSquare={ () => { updateSquare( 2, 2 ) }}/>
+        </div>
+        {/* Row 3 */}
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   )
 }
