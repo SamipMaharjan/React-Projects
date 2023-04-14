de81a9a533ad4cd44b13e3519f6b895691d8b48b
Samip_Maharjan <adsrb15@gmail.com>
2023-04-14 08:45:36 +0545

    starting the library app

--- Crypto Pricing App/src/Pages/Show.jsx
+++ Crypto Pricing App/src/Pages/Show.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-export default function Home() {
+export default function Show() {
   return (
     <div>
-      Home page 
+      Show page
     </div>
   )
 }
--- Crypto Pricing App/src/main.jsx
+++ Crypto Pricing App/src/main.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './Pages/Home';
+import Show from './Pages/Show'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route index element = {<Home/>}/>
+        <Route path = "/:id" element={<Show/>}/>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
