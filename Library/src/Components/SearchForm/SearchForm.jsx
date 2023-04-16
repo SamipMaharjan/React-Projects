66719d2321e60ecfc0ef10d356fb3386cce5caed
Samip_Maharjan <adsrb15@gmail.com>
2023-04-16 15:56:10 +0545

    libnrary app

---
+++ Library/src/Components/SearchForm/SearchForm.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './SearchForm.css'
+
+export default function SearchForm() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
---
+++ Library/src/main.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './Pages/Home/Home'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <>
+      {/* <BrowserRouter>
+        <Routes>
+          <Route index element = {<Home/>}/>
+        </Routes>
+      </BrowserRouter> */}
+      <div className="font-bold">
+        Hello World atailwind.
+      </div>
+    </>
+  </React.StrictMode>,
+)
