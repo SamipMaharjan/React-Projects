a80a1773bee4d089c335bd1cabc865f54f942c37
Samip_Maharjan <adsrb15@gmail.com>
2023-04-23 07:21:16 +0545

    updating the sql and php of the weather app

--- wether/server.php
+++ wether/server.php
@@ -3,17 +3,26 @@
     $username = "root";
     $password = "";
     $dbName = "php";
+
     $connection = mysqli_connect( $localhost, $username, $password, $dbName );
 
     if( $connection -> connect_error ){
         echo "Connection error with the database :)";
     }
 
+    //Storing the url of the api in variable
     $url = "https://api.openweathermap.org/data/2.5/weather?lat=51.4800285&lon=-0.6125716&appid=9964006f86896128a73526b1d2b01786";
+
+
+    function store_data_in_database( $url, $connection ){
+
+    //fetching data from the api and storing it in a variable
     $windsor_data = file_get_contents( $url );
 
+    //Converting the data from json format to associative format.  
     $associative_format = json_decode( $windsor_data, true );
-
+    
+    //Storing relevant data from the associative array in their respective variables 
     $weatherDescription = $associative_format['weather'][0]['description'];
     $temperature = ($associative_format['main']['temp'])-273.15;
     $feels_like = ($associative_format['main']['feels_like'])-273.15;
@@ -23,17 +32,32 @@
     $weatherState = $associative_format['weather'][0]['main'];
     $cityName = $associative_format['name'];
 
-
-//
+    //sql code to insert all the required data into the sql database
     $sql = "INSERT INTO windsor(cityName, weather_description, temperature, feels_like, pressure, humidity, windSpeed, weatherState) VALUE('".$cityName."','".$weatherDescription."','".$temperature."','".$feels_like."','".$pressure."','".$humidity."','".$windSpeed."','".$weatherState."')";
 
+    //sql code to delete all data from the sql database
     $sql2 = "DELETE FROM windsor;";
 
-    echo $weatherDescription,"<br>",$weatherState,"<br>",$temperature,"<br>", $feels_like,"<br>", $pressure,"<br>",$humidity,"<br>",$windSpeed;
+    mysqli_query( $connection, $sql2 );
+    mysqli_query( $connection, $sql );
+
+    }
+    
+    store_data_in_database( $url, $connection );
+
+
+
+    function get_data_from_database( $connection ){    
+        $sql3 = "SELECT * FROM windsor";
+        $SQL_data = mysqli_query( $connection, $sql3 );
+
+        data [] = 
+    }
+
 
 
     
-    mysqli_query( $connection, $sql2 );
-    mysqli_query( $connection, $sql );
+    // echo $weatherDescription,"<br>",$weatherState,"<br>",$temperature,"<br>", $feels_like,"<br>", $pressure,"<br>",$humidity,"<br>",$windSpeed;
+
 
 ?>
