4ab9f847323ca747012cab06c4a0c0935ef06bb7
Samip_Maharjan <adsrb15@gmail.com>
2023-05-02 21:29:43 +0545

    Working with Header Component

---
+++ Library with Tailwind/src/App.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from 'react'
+import {  BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './Pages/Home/Home'
+  import About from '../../Library/src/Pages/About/About'
+  import BookList from "./Components/BookList/BookList"
+  import BookDetails from "./Components/BookDetails/BookDetails"
+
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path = "/" element = {<Home />} >
+            <Route path = 'about' element = {<About />} />
+            <Route path = 'book' element = {<BookList/>} />
+            <Route path = 'book/:id' element = {<BookDetails/>} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
--- Library with Tailwind/src/Components/Header/Header.jsx
+++ Library with Tailwind/src/Components/Header/Header.jsx
@@ -3,15 +3,22 @@ import './Header.css'
 import NavBar from '../NavBar/NavBar'
 import SearchForm from '../SearchForm/SearchForm'
 
+
 export default function Header() {
   return (
     <div className='holder'>
         <header className='header'>
           <NavBar/>
-          <div className='header-content flex flex-c text-center text-white'> 
+          <div className='header-content flex flex-col text-center text-black'> 
             <h2 className='header-title text-capitalize '>Find your book of choise</h2>
             <br />
+
+            <p className='header-text fs-18 fw-3'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi accusamus eos ullam, quis itaque illo hic cumque ratione a incidunt velit ut repellendus. Tempore deleniti expedita sit eligendi minus a?</p>
+
+            <SearchForm/>
           </div>
+
+
         </header>
     </div>
   )
