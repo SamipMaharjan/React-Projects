e4b6ff4fcd2a68f7b355f0fe58daa18877113c8d
Samip_Maharjan <adsrb15@gmail.com>
2023-05-03 21:50:36 +0545

    custom classes for tailwind

--- Library with Tailwind/src/App.jsx
+++ Library with Tailwind/src/App.jsx
@@ -1,10 +1,10 @@
 import React, { useState } from 'react'
 import {  BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './Pages/Home/Home'
-  import About from '../../Library/src/Pages/About/About'
-  import BookList from "./Components/BookList/BookList"
-  import BookDetails from "./Components/BookDetails/BookDetails"
-
+import About from '../../Library/src/Pages/About/About'
+import BookList from "./Components/BookList/BookList"
+import BookDetails from "./Components/BookDetails/BookDetails"
+import './App.css';
 
 function App() {
   return (
