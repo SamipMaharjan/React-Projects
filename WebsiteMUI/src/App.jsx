0bad530012b0f6ee866ec55b2f4ea0614d3efbe8
SamipMaharjan <adsrb15@gmail.com>
2023-05-28 21:49:29 +0545

    materialUI

--- WebsiteMUI/src/App.jsx
+++ WebsiteMUI/src/App.jsx
@@ -1,9 +1,15 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Layout from './components/Layout/Layout'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './Pages/Home'
+import About from './Pages/About'
+import Contact from './Pages/Contact'
+import Menu from './Pages/Menu'
+import PageNotFound from './Pages/PageNotFound'
+import Header from './components/Layout/Header';
+
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -14,16 +20,16 @@ function App() {
     <BrowserRouter>
 
       <Routes>
-
+        
         <Route index element = { <Home /> } />
-        <Route path='' />
+        <Route path='/about' element = { <About />} />
+        <Route path='/contact' element = { <Contact />} />
+        <Route path='/menu' element = { <Menu />} />
+        <Route path='*' element = { <PageNotFound />} />
+         
       </Routes>
     
     </BrowserRouter>
-    <h1>Restaurant Website</h1>
-        <Layout>
-          SamipMahrjan
-        </Layout>
     </>
   )
 }
--- WebsiteMUI/src/components/Layout/Header.jsx
+++ WebsiteMUI/src/components/Layout/Header.jsx
@@ -1,9 +1,68 @@
 import React from 'react'
+import { Box, AppBar, Typography, Toolbar, Button } from '@mui/material'
+import StorefrontIcon from '@mui/icons-material/Storefront';
+import { Link } from 'react-router-dom'
 
 export default function Header() {
   return (
-    <div>
-      Header
-    </div>
+    <>
+     <Box>
+      <AppBar component={'nav'} sx={ {bgcolor: 'black'} }>
+        <Toolbar>
+          
+          <Typography color={'goldenrod'} component={'div'} variant='h6' sx={{flexGrow : 1}}>   
+          <StorefrontIcon />
+            My Restaurant 
+          </Typography>  
+          
+          <Box>
+          <Button variant="text" component={Link} to="/">
+            Home
+          </Button>
+
+          <Button variant="text" component={Link} to="/about">
+            About
+          </Button>
+
+          <Button variant="text" component={Link} to="/contact">
+            Contact
+          </Button>
+
+          <Button variant="text" component={Link} to="/Menu">
+            Menu 
+          </Button>
+
+          </Box>
+          
+
+      {/* <Box>
+            <ul>
+
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+
+              <li>
+              <Link to='/about'>About</Link>
+              </li>
+              
+              <li>
+              <Link to='/contact'>Contact</Link>
+              </li>
+
+              <li>
+              <Link to='/menu'>Menu</Link>  
+              </li>
+
+            </ul>
+      </Box> */}
+          
+        </Toolbar>
+      
+      
+
+      </AppBar>
+     </Box>
+    </>
   )
 }
