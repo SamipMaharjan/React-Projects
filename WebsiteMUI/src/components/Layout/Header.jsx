3bb48ca6833eb13e8a48ba875234cf627543e8d2
SamipMaharjan <adsrb15@gmail.com>
2023-05-29 23:37:50 +0545

    Drawers and sx sm xm md lg xl

--- WebsiteMUI/src/components/Layout/Header.jsx
+++ WebsiteMUI/src/components/Layout/Header.jsx
@@ -1,41 +1,65 @@
-import React from 'react'
-import { Box, AppBar, Typography, Toolbar, Button } from '@mui/material'
-import StorefrontIcon from '@mui/icons-material/Storefront';
-import { Link } from 'react-router-dom'
+import React,{ useState } from "react";
+import {
+  Box,
+  AppBar,
+  Typography,
+  Toolbar,
+  Button,
+  IconButton,
+} from "@mui/material";
+import StorefrontIcon from "@mui/icons-material/Storefront";
+import MenuIcon from "@mui/icons-material/Menu";
+import { Link } from "react-router-dom";
 
 export default function Header() {
+  const [ mobileOpen, setMobileOpen ] = useState(false);
+  const drawer = {
+    
+  }
+
   return (
     <>
-     <Box>
-      <AppBar component={'nav'} sx={ {bgcolor: 'black'} }>
-        <Toolbar>
-          
-          <Typography color={'goldenrod'} component={'div'} variant='h6' sx={{flexGrow : 1}}>   
-          <StorefrontIcon />
-            My Restaurant 
-          </Typography>  
-          
-          <Box>
-          <Button variant="text" component={Link} to="/">
-            Home
-          </Button>
+      <Box>
+        <AppBar component={"nav"} sx={{ bgcolor: "black" }}>
+          <Toolbar>
+            <IconButton>
+              <MenuIcon
+                sx={{
+                  color: "white",
+                  fontSize: 34,
+                  display: { xs: "block", sm: "none" },
+                }}
+              ></MenuIcon>
+            </IconButton>
+            <Typography
+              color={"goldenrod"}
+              component={"div"}
+              variant="h6"
+              sx={{ flexGrow: 1 }}
+            >
+              <StorefrontIcon />
+              My Restaurant
+            </Typography>
 
-          <Button variant="text" component={Link} to="/about">
-            About
-          </Button>
+            <Box sx={{ display: { xs: "none", sm: "block" } }}>
+              <Button variant="text" component={Link} to="/">
+                Home
+              </Button>
 
-          <Button variant="text" component={Link} to="/contact">
-            Contact
-          </Button>
+              <Button variant="text" component={Link} to="/about">
+                About
+              </Button>
 
-          <Button variant="text" component={Link} to="/Menu">
-            Menu 
-          </Button>
+              <Button variant="text" component={Link} to="/contact">
+                Contact
+              </Button>
 
-          </Box>
-          
+              <Button variant="text" component={Link} to="/Menu">
+                Menu
+              </Button>
+            </Box>
 
-      {/* <Box>
+            {/* <Box>
             <ul>
 
               <li>
@@ -56,13 +80,9 @@ export default function Header() {
 
             </ul>
       </Box> */}
-          
-        </Toolbar>
-      
-      
-
-      </AppBar>
-     </Box>
+          </Toolbar>
+        </AppBar>
+      </Box>
     </>
-  )
+  );
 }
