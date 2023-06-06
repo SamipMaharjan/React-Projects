6175cbed93ed808aa5b25fa8b563c5f6608a7cf5
SamipMaharjan <adsrb15@gmail.com>
2023-06-06 17:30:45 +0545

    making new project with tailwind

--- EcommereceSite-2/src/App.jsx
+++ EcommereceSite-2/src/App.jsx
@@ -1,16 +1,24 @@
 import { useState } from "react";
 import { Button, Container, Typography } from "@mui/material";
+import React from 'react'; 
+import { ThemeProvider } from '@emotion/react';
+import theme from "./Styles";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
+    <ThemeProvider theme={ theme }>
+
       <Container maxWidth='xl' sx={{
-          background: '#fff'
-        }}>
-        <Button variant="outlined">Text</Button>
+            background: '#fff'
+          }}>
+          <Button variant="contained">Text</Button>
       </Container>
+    
+    </ThemeProvider>
+      
 
       {
         /*
