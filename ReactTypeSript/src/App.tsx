4183389b9ae240da4764ba0171f3520181761488
SamipMaharjan <adsrb15@gmail.com>
2023-06-22 21:48:13 +0545

    typescriptript in react

---
+++ ReactTypeSript/src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import { Greet } from './components/Greet'
+
+function App() {
+
+  return (
+    <>
+     <Greet name={"saip"} />
+    </>
+  )
+}
+
+export default App
---
+++ ReactTypeSript/src/components/Greet.tsx
@@ -0,0 +1,7 @@
+export const Greet = ({name: string}) => {
+    return (
+        <div>
+            <h2>Welcome {name} :</h2>
+        </div>
+    )
+}
